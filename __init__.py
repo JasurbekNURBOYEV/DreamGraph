1ceea4b2bc62dbc8db403fc80507890af575ba13
Future Dreams <bluestacks6523@gmail.com>
2018-05-17 23:54:05 +0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,9 +1,19 @@
 """
-The Python module for Telegraph API
-For bug reports: t.me/FutureDreams
+Dreamgraph - The Python module for Telegraph API.
+Copyright (C) 2018  Jasur NURBOEV <https://github.com/JasurbekNURBOYEV>
 
- - The Telegraph
- - version: 1.0.0
+Dreamgraph is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Dreamgraph is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 from .api import LogIn, NewAccount
