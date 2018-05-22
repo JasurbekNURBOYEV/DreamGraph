c0fd79162fa49267e8ddd01eb590c737db70cec6
Future Dreams <bluestacks6523@gmail.com>
2018-05-22 21:30:43 +0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dreamgraph',
+  packages = ['dreamgraph'],
+  version = '1.0.0',
+  description = 'The Python module for Telegraph API',
+  author = 'Jasur Nurboev',
+  author_email = 'bluestacks6523@gmail.com',
+  url = 'https://github.com/JasurbekNURBOYEV/DreamGraph',
+  download_url = 'https://github.com/JasurbekNURBOYEV/DreamGraph/archive/1.0.0.tar.gz',
+  keywords = ['telegraph', 'telegraph-api', 'python-module', 'dreamgraph'],
+  classifiers = [],
+)
