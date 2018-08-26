29f07aaec061815f6b472deb134dcda667f893a2
Future Dreams <bluestacks6523@gmail.com>
2018-08-27 01:32:02 +0500

    [NEW] [FUNC] start()
    We are launching the new version with start() method suggested by Poolitzer. It'll handle all account creating/logging in stuff.

--- dreamgraph/api.py
+++ dreamgraph/api.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 """
 DreamGraph - The Python module for Telegraph API.
 Copyright (C) 2018  Jasur NURBOEV <https://github.com/JasurbekNURBOYEV>
@@ -18,7 +18,7 @@ import json
 import requests as r
 from .params import API_URL
 from .objects import Account
-
+import re
 
 def LogIn(access_token):
     """
@@ -46,7 +46,79 @@ def NewAccount(short_name, author_name=None, author_url=None):
     else:
         error = json_object['error']
         raise ValueError('Telegraph API raised an error: {}'.format(error))
-        
+
+
+def extract_from_file():
+    """
+    :return: string object representing the access_token
+    """
+    data = open('./config.ini', 'r').read()
+    pattern = '[a-zA-Z0-9]{60}'
+    result = re.findall(pattern, data)
+    if len(result) > 0:
+        return result[0]
+    else:
+        return False
+
+
+def store_access_token(access_token):
+    """
+    :param access_token: access_token of an account
+    :return: boolean
+    """
+    template = '[dreamgraph]\naccess_token = {access_token}'
+    data = template.format(access_token=access_token)
+    open('./config.ini', 'w').write(data)
+    return True
+
+
+def register():
+
+    """
+    :return: Account object
+    """
+    short_name = None
+    author_name = None
+    author_url = None
+
+    short_name = input('Enter short name for your account (you can change it later): ')
+    while len(short_name) == 0 or len(short_name.replace(' ', '')) == 0:
+        short_name = input('''Short name is required in order to create an account.
+Please, choose one for yourself.
+(short name cannot contain only white spaces): ''')
+
+    author_name = input('''Enter a name for you, it will be shown in every page you create.
+You can change it later.
+Author name is an optional data, so you can skip this step by pressing the Enter key: ''')
+    if len(author_name) == 0 or len(author_name.replace(' ', '')) == 0:
+        author_name = None
+
+    if author_name:
+        author_url = input('''Enter URL for your account, it will make your author name clickable.
+You can change it later.
+Author URL is an optional data, so you can skip this step by pressing the Enter key: ''')
+        if len(author_url) == 0 or len(author_url.replace(' ', '')) == 0:
+            author_url = None
+
+    new_account = NewAccount(short_name=short_name, author_name=author_name, author_url=author_url)
+    store_access_token(new_account.access_token)
+    return new_account
+
+
+def start():
+    """
+    :return: Account object
+    """
+    try:
+        access_token = extract_from_file()
+        if access_token:
+            return LogIn(access_token)
+        else:
+            return register()
+    except:
+        return register()
+
+
 def msg_to_node(data, types='msg'):
     """
     Function to make Node elements from Message object.
